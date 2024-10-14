38588cbaa713c1cb27780593508f4c6b0d715fb6
Eyüphan Kaygusuz <eyuphankaygusuz@gmail.com>
2024-10-14 20:53:47 +0300

    PostProcessing Tests

--- Assets/Scripts/PlayerScripts/PlayerScan.cs
+++ Assets/Scripts/PlayerScripts/PlayerScan.cs
@@ -41,7 +41,7 @@ public class PlayerScan : MonoBehaviour, IInputHandler
 	private float _initialChromaticAberration = 0f;
 	private float _targetChromaticAberration = 1f;
 
-	private Color _initialColor = new Color(1f, 1f, 1f);
+	private Color _initialColor = new Color(200f / 255f, 56f/255f, 56f / 255f);
 	private Color _targetColor = new Color(210f / 255f, 0f, 0f);
 
 
